3081a60b04b78c2207969717afea1a8386cc73c4
Ben Anandappa <benanandappa@college.harvard.edu>
2017-12-04 14:00:06 -0500

    Update Slides

--- publish.sh
+++ publish.sh
@@ -14,7 +14,7 @@ for f in "${arr[@]}"; do
    filename="${filename%.*}"
 
    # Convert the Notebook to HTML
-   jupyter-nbconvert --to html Notebooks/"$filename".ipynb --template=markdown.tpl
+   jupyter-nbconvert --to html Notebooks/"$filename".ipynb --template markdown.tpl
    # Move to the Html directory
    mv Notebooks/"$filename".html  Html/"$filename".html
 
@@ -24,9 +24,14 @@ for f in "${arr[@]}"; do
    mv Notebooks/"$filename".slides.html  Slides/"$filename".html
 
    # Convert the Notebook to Markdown
-   jupyter-nbconvert --to markdown --template=markdown.tpl Notebooks/"$filename".ipynb
+   jupyter-nbconvert --to markdown --template markdown.tpl Notebooks/"$filename".ipynb
    # Move to the Markdown directory
    mv Notebooks/"$filename".md  Markdown/"$filename".md
+
+   # Convert the Notebook to Latex
+   jupyter-nbconvert --to latex Notebooks/"$filename".ipynb
+   # Move to the Tex directory
+   mv Notebooks/"$filename".tex  Tex/"$filename".tex
 
    # Convert the Notebook to Pdf
    cp Notebooks/"$filename".ipynb src/"$filename".ipynb
